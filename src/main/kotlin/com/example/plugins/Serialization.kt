738a0d3eec817d8e51559d78981b90c86fdd3e0b
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-10-16 19:19:39 +0200

    aumento 1

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -13,18 +13,31 @@ fun Application.configureSerialization() {
     }
     routing {
         get("/characters") {
-            val characters = listOf(
-                Character(
-                    id = 8,
-                    name = "Seele",
-                    rarity = 5,
-                    path = "Hunt",
-                    element = "Quantum",
-                    release = "2023-04-26T00:00:00.000Z",
-                    introduction = "A resident of the Underworld and the backbone of Wildfire. She goes by the alias \"Babochka.\" She has a frank personality, but there is a delicate and sensitive hidden side to her deep in her heart.",
-                    img = "https://static.wikia.nocookie.net/houkai-star-rail/images/5/58/Character_Seele_Splash_Art.png/revision/latest/scale-to-width-down/1000?cb=20230706032216"
+            val characters =
+                listOf(
+                    Character(
+                        id = 1,
+                        name = "Trailblazer",
+                        rarity = 5,
+                        path =  "Destruction",
+                        element =  "Physical",
+                        release =  "2023-04-26T00:00:00.000Z",
+                        introduction = "A boy/girl who boarded the Astral Express. They chose to travel with the Astral Express to eliminate the dangers posed by the Stellaron.",
+                        img = "https://static.wikia.nocookie.net/houkai-star-rail/images/c/cc/Character_Trailblazer_Splash_Art.png"
+                    ),
+                    Character(
+                        id = 8,
+                        name = "Seele",
+                        rarity = 5,
+                        path = "Hunt",
+                        element = "Quantum",
+                        release = "2023-04-26T00:00:00.000Z",
+                        introduction = "A resident of the Underworld and the backbone of Wildfire. She goes by the alias \"Babochka.\" She has a frank personality, but there is a delicate and sensitive hidden side to her deep in her heart.",
+                        img = "https://static.wikia.nocookie.net/houkai-star-rail/images/5/58/Character_Seele_Splash_Art.png/revision/latest/scale-to-width-down/1000?cb=20230706032216"
+                    ),
+
                 )
-            )
+
             call.respond(characters) // Responde con la lista de personajes
         }
     }
